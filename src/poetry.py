6b87e6196d84b41ea183353e40a73943c6e4f1f8
Hannes Struss <x@hannesstruss.de>
2011-04-10 22:02:03 +0200

    use gaussian distribution to select following word

--- src/poetry.py
+++ src/poetry.py
@@ -2,6 +2,7 @@
 
 import re
 import random
+import math
 
 PATH = "input_utf8.txt"
 
@@ -21,16 +22,47 @@ def read_input(path):
 	
 	return result
 
+def get_most_common_follower(followers):
+	result = None
+	max_count = 0
+	for follower, count in followers.iteritems():
+		if count > max_count:
+			result = follower
+			max_count = count
+	return result
+
+def get_gaussian_distributed_follower(follower_list):
+	rnd = min(len(follower_list) - 1, int(abs(random.gauss(0, 1)) * len(follower_list)))
+	return follower_list[rnd]
+
 def create_poem(db):
 	word = random.choice(db.keys())
 	result = []
 	while len(result) < 20:
 		result.append(word)
-		word = random.choice(db[word].keys()) 
+		#word = random.choice(db[word].keys())
+		word = get_gaussian_distributed_follower(get_sorted_follower_list(db[word]))
 		
 	return " ".join(result)
+
+def get_sorted_follower_list(followers):
+	return map(lambda x: x["k"], sorted(
+		[{"k": key, "v": value} for key,value in followers.iteritems()],
+		key=lambda a: a["v"],
+		reverse=True
+	))
+
+def get_pretty_follower_list(word, db):
+	followers = db[word]
+	result = [word]
+	for x in get_sorted_follower_list(followers):
+		result.append("%s\t\t\t%s" % (x["word"], x["count"]))
+		
+	return "\n".join(result)
 	
 if __name__ == '__main__':
 	input = read_input(PATH)
 	for i in xrange(10):
 		print create_poem(input)
+		
+	print get_sorted_follower_list(input["sollst"])
